2c659d48da62b085b396c67cc941b80459c6dcd7
WimFontys <437045@student.fontys.nl>
2024-05-12 13:58:28 +0200

    Right to be forgotten functionality fix
    Modifying/Transactional

--- src/main/java/y/userservice/repositories/UserFollowRepository.java
+++ src/main/java/y/userservice/repositories/UserFollowRepository.java
@@ -1,7 +1,9 @@
 package y.userservice.repositories;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 import y.userservice.domain.entities.UserFollowEntity;
 import y.userservice.domain.entities.UserFollowId;
 
@@ -18,6 +20,8 @@ public interface UserFollowRepository extends CrudRepository<UserFollowEntity, U
         @Query("SELECT u.id.followedId FROM UserFollowEntity u WHERE u.id.followerId = ?1")
         List<Integer> getFollowing(Integer followerId);
 
+        @Modifying
+        @Transactional
         @Query("DELETE FROM UserFollowEntity u WHERE u.id.followerId = ?1 OR u.id.followedId = ?1")
         void deleteAllFollowRelationships(Integer userId);
 }
